07192633ea3c00ef7f6b9ec953fe3b8b02f02152
Sabina Dinu <dinu_sabina18@yahoo.com>
2023-02-12 23:46:12 +0200

    add shopping edit component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ShoppingComponent } from './shopping/shopping.component';
+import { ShoppingComponent } from './shopping-list/shopping.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShoppingComponent
+    ShoppingComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
