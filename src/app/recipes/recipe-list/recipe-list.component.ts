38f27fd508627d1ee79c7d29c2471103869ad6ea
Sabina Dinu <dinu_sabina18@yahoo.com>
2023-02-13 00:41:09 +0200

    added content to the recipe component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Recipe} from "../recipes.model";
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements  OnInit{
+  recipes: Recipe[] = [
+    new Recipe("A test name", "test descrip", "path test")
+  ];
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
 
 }
